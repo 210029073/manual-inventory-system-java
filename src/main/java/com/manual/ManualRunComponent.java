196033e4d50a0267511dc279a67fa6614abaef6f
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-04 17:45:16 +0000

    First draft of Dashboard and Login form.

--- src/main/java/com/manual/ManualRunComponent.java
+++ src/main/java/com/manual/ManualRunComponent.java
@@ -1,6 +1,10 @@
 package com.manual;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class ManualRunComponent extends Application {
@@ -11,6 +15,12 @@ public class ManualRunComponent extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/com.manual.main/login.fxml"));
+        HBox container = fxmlLoader.load();
+        stage.setScene(new Scene(container));
+        stage.setResizable(false);
+        stage.setTitle("Inventory Management System");
+        stage.show();
     }
 }
