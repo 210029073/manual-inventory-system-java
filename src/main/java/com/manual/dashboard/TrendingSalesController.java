265dfb537405f7289c91433abe130f28f5664162
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-03-03 10:34:40 +0000

    Added Annual Sales revenue gained in TrendingSalesController.java

--- src/main/java/com/manual/dashboard/TrendingSalesController.java
+++ src/main/java/com/manual/dashboard/TrendingSalesController.java
@@ -7,19 +7,26 @@ import com.manual.model.ProductCollections;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 public class TrendingSalesController {
 
     private final OrderCollections orderCollections;
+    private final VBox annualSalesContainer;
     @FXML
     private VBox container;
 
@@ -29,16 +36,41 @@ public class TrendingSalesController {
 
     private ProductCollections productCollections;
 
+    private HBox statisticsContainer;
+
+    private Text txtAnnualSales;
+    private Text txtAnnualSalesMsg;
+
     public TrendingSalesController() {
         this.totalCarBrand = new HashMap<>();
         this.dataSets = new HashMap<>();
         this.productCollections = new ProductCollections();
 
         orderCollections = new OrderCollections();
+        this.txtAnnualSales = new Text();
+        annualSalesContainer = new VBox();
+        this.statisticsContainer = new HBox();
     }
 
     public void initialize() {
         prepareTrendingSalesGraph();
+
+        //set text field
+        setAnnualSalesText();
+    }
+
+    private void setAnnualSalesText() {
+        annualSalesContainer.setSpacing(10);
+        annualSalesContainer.paddingProperty().setValue(new Insets(25));
+        txtAnnualSalesMsg = new Text("Annual Sale Summary");
+        txtAnnualSalesMsg.setFont(new Font("Arial Bold", 24));
+        annualSalesContainer.getChildren().add(txtAnnualSalesMsg);
+        annualSalesContainer.getChildren().add(this.txtAnnualSales);
+        txtAnnualSales.setFont(new Font("Arial", 22));
+        txtAnnualSales.setVisible(true);
+        statisticsContainer.setVisible(true);
+        this.statisticsContainer.getChildren().add(annualSalesContainer);
+        this.container.getChildren().add(statisticsContainer);
     }
 
     private void prepareTrendingSalesGraph() {
@@ -63,6 +95,8 @@ public class TrendingSalesController {
     }
 
     private void plotData(BarChart<String, Number> barChart, OrderCollections orderCollections) {
+        Float salesVal = 0.00F;
+
         for (Order o : orderCollections.getPastOrders()) {
             XYChart.Series<String, Number> productBrand = dataSets.get(o.getProduct().getProductBrand());
 
@@ -70,7 +104,10 @@ public class TrendingSalesController {
 
             int oldValue = (int) totalCarBrand.get(o.getProduct().getProductBrand());
             totalCarBrand.replace(o.getProduct().getProductBrand(), oldValue + 1);
-
+            if(LocalDate.parse(o.getOrderDate().toString()).getYear() == LocalDate.now().getYear()) {
+                salesVal += o.getProduct().getProductPrice();
+                this.txtAnnualSales.setText("£" + salesVal);
+            }
             dataSets.replace(o.getProduct().getProductBrand(), productBrand);
             barChart.getData().remove(productBrand);
             barChart.getData().add(productBrand);
@@ -89,8 +126,28 @@ public class TrendingSalesController {
     }
 
     @FXML
-    public void btnExit() {
+    public void refreshScreen() {
 
+    }
+
+    @FXML()
+    public void viewMainMenu() {
+
+    }
+
+    @FXML
+    public void viewPastOrders() {
+
+    }
+
+    @FXML
+    public void viewAboutInfo() {
+        
+    }
+
+    @FXML
+    public void btnExit() {
+        this.container.getScene().getWindow().hide();
     }
 
 }
